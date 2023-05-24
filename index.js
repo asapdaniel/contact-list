b804e90fcfde7e8eced90cb00ec664e5d507cb37
Daniel Florez <daflorez406@misena.edu.co>
2023-05-23 23:52:44 -0500

    se crea lista

---
+++ index.js
@@ -0,0 +1,10 @@
+ let contactsList = {
+    "nameAndLastName": ["Daniel Flórez"],
+    "phoneNumber": [3015624017],
+    "email": ["daniele@gmail.com"],
+    "sports" : ["basketball", "mma", "soccer"]
+ 
+ };
+ let agregar = contactsList.email.push("danielstoff0");
+ contactsList.pop();
+console.log(contactsList);
